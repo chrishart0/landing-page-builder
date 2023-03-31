4b80e501e75121fec64d6a4a4ee9ac3713e7b7f0
chrishart0 <christianphart0@gmail.com>
2023-03-30 21:04:16 -0400

    Get build to pass

--- sanityClient.js
+++ sanityClient.js
@@ -4,5 +4,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'uoq5m591', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
-  useCdn: false // `false` if you want to ensure fresh data
+  useCdn: false, // `false` if you want to ensure fresh data
+  apiVersion: '2021-08-31'
 })
