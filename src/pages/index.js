13aaa8d0f55b7fb9eebb306bc7c3d7d2eccdb07e
chrishart0 <christianphart0@gmail.com>
2023-03-30 21:14:36 -0400

    Working

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,34 +3,49 @@ import Link from 'next/link'
 import groq from 'groq'
 import client from '../../sanityClient'
 
-const Index = ({posts}) => {
-    return (
-      <div>
-        <h1>Welcome to a blog!</h1>
-        {posts.length > 0 && posts.map(
-          ({ _id, title = '', slug = '', publishedAt = '' }) =>
-            slug && (
-              <li key={_id}>
-                <Link href={`/post/${encodeURIComponent(slug.current)}`}>
-                  {title}
-                </Link>{' '}
-                ({new Date(publishedAt).toDateString()})
-              </li>
-            )
-        )}
-      </div>
-    )
+const Index = ({ posts, formattedDate }) => {
+  return (
+    <div>
+      <h1>Welcome to a blog!</h1>
+      {posts.length > 0 && posts.map(
+        ({ _id, title = '', slug = '', publishedAt = '' }) =>
+          slug && (
+            <li key={_id}>
+              <Link href={`/post/${encodeURIComponent(slug.current)}`}>
+                {title}
+              </Link>{' '}
+              ({new Date(publishedAt).toDateString()})
+            </li>
+          )
+      )}
+      <p>
+        This page is server-side rendered. It was rendered on {formattedDate}.
+      </p>
+    </div>
+  )
 }
 
-export async function getStaticProps() {
-    const posts = await client.fetch(groq`
-      *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
-    `)
-    return {
-      props: {
-        posts
-      }
+export async function getServerSideProps() {
+  const renderDate = Date.now();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+    timeStyle: "long",
+  }).format(renderDate);
+  console.log(
+    `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
+  );
+
+  const posts = await client.fetch(groq`
+  *[_type == "post" && publishedAt < now()] | order(publishedAt desc)
+`)
+  return {
+    props: {
+      posts,
+      formattedDate
     }
+  }
+
+  return { props: { formattedDate } };
 }
 
 export default Index
